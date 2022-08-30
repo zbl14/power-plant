34c919f8dcc96c389806d06b08d506dbb2a3513b
zbl14 <ifthereisoneday@gmail.com>
2022-08-29 22:36:37 -0700

    add soil/blue food

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+import { stateControl, blueFood } from './js/plant';
+
+$(document).ready(function() {
+
+  // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
+  
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
+  
+  // This function doesn't actually do anything useful in this application — it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+  
+  $('#show-state').click(function() {
+    // We just need to call stateControl() without arguments to see our current state.
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
---
+++ src/js/plant.js
@@ -0,0 +1,33 @@
+// This function stores our state.
+
+const initialState = { soil: 0, light: 0, water: 0 };
+
+const storeState = () => {
+  let currentState = initialState;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  };
+};
+
+export const stateControl = storeState();
+
+// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    });
+  };
+};
+
+// We create four functions using our function factory. We could easily create many more.
+
+export const feed = changeState("soil")(1);
+export const blueFood = changeState("soil")(5);
+
+export const hydrate = changeState("water")(1);
+export const superWater = changeState("water")(5);
