4d9a4b947e912e834b0c5c493f87cd34f4578ed2
zbl14 <ifthereisoneday@gmail.com>
2022-08-29 22:12:45 -0700

    initial commit after template adjustment

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: ' ',
+      title: 'Power Plant',
       template: './src/index.html',
       inject: 'body'
     }),
